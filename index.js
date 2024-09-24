c1175537203a29f1d094e206ced5464e3a13792f
Sajeeb <mdakibhasansajeeb@gmail.com>
2024-09-24 14:09:52 +0600

    Donate Now button properly worked for all the three carts

--- index.js
+++ index.js
@@ -1,31 +1,73 @@
 // Common Shared Function Start
-function getInputFieldById(id){
-const inputValue = document.getElementById(id).value;
-const inputNumber = parseFloat(inputValue);
-return inputNumber;
+function getInputFieldById(id) {
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+    return inputNumber;
 }
 
-function getElementById(id){
+function getTextFieldValueById(id) {
     const inputValue = document.getElementById(id).innerText;
     const inputNumber = parseFloat(inputValue);
     return inputNumber;
-    }
-    
+}
 // Common Shared Function End
 
+// Noakhali
+document.getElementById('btn-donate').addEventListener('click', function (event) {
+    event.preventDefault();
+    const addAmount = getInputFieldById('input-add-amount');
 
-document.getElementById('btn-donate').addEventListener('click', function(event){
-event.preventDefault();
-const addAmount = getInputFieldById('input-add-amount');
+    if (addAmount < 0 || isNaN(addAmount)) {
+        alert('Failed To Add Money');
+    }
 
-if(addAmount < 0 || isNaN(addAmount)){
-    alert('Failed To Add Money');
-}
-    
-else{
-    const balance = getElementById('account-balance');
-    const newBalance = balance + addAmount;
-    document.getElementById('account-balance').innerText = newBalance + ' BDT';
-}
+    else {
+        const balance = getTextFieldValueById('account-balance');
+        const mainBalance = getTextFieldValueById('account-main-balance');
+        const newBalance = balance + addAmount;
+        const newMainBalance = mainBalance - addAmount;
+        document.getElementById('account-balance').innerText = newBalance + ' BDT';
+        document.getElementById('account-main-balance').innerText = newMainBalance + ' BDT';
+    }
+
+})
+
+// Feni
+document.getElementById('btn-donate-feni').addEventListener('click', function (event) {
+    event.preventDefault();
+    const addAmount = getInputFieldById('input-add-amount-feni');
+
+    if (addAmount < 0 || isNaN(addAmount)) {
+        alert('Failed To Add Money');
+    }
+
+    else {
+        const balance = getTextFieldValueById('account-balance-feni');
+        const mainBalance = getTextFieldValueById('account-main-balance');
+        const newBalance = balance + addAmount;
+        const newMainBalance = mainBalance - addAmount;
+        document.getElementById('account-balance-feni').innerText = newBalance + ' BDT';
+        document.getElementById('account-main-balance').innerText = newMainBalance + ' BDT';
+    }
+
+})
+
+// Quota
+document.getElementById('btn-donate-quoata').addEventListener('click', function (event) {
+    event.preventDefault();
+    const addAmount = getInputFieldById('input-add-amount-quoata');
+
+    if (addAmount < 0 || isNaN(addAmount)) {
+        alert('Failed To Add Money');
+    }
+
+    else {
+        const balance = getTextFieldValueById('account-balance-quoata');
+        const mainBalance = getTextFieldValueById('account-main-balance');
+        const newBalance = balance + addAmount;
+        const newMainBalance = mainBalance - addAmount;
+        document.getElementById('account-balance-quoata').innerText = newBalance + ' BDT';
+        document.getElementById('account-main-balance').innerText = newMainBalance + ' BDT';
+    }
 
 })
